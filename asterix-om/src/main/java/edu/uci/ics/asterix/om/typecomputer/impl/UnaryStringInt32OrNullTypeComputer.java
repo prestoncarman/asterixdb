b3de19cafd4dba0a5985733fa26671bcdd790d9f
ilovesoup <ilovesoup@eaa15691-b419-025a-1212-ee371bd00084>
2013-01-24 18:39:58 +0000

    resubmit for code review
    git-svn-id: https://asterixdb.googlecode.com/svn/branches/asterix_soup_staging@1086 eaa15691-b419-025a-1212-ee371bd00084

--- asterix-om/src/main/java/edu/uci/ics/asterix/om/typecomputer/impl/UnaryStringInt32OrNullTypeComputer.java
+++ asterix-om/src/main/java/edu/uci/ics/asterix/om/typecomputer/impl/UnaryStringInt32OrNullTypeComputer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009-2010 by The Regents of the University of California
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * you may obtain a copy of the License from
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.uci.ics.asterix.om.typecomputer.impl;
 
 import java.util.ArrayList;
@@ -17,6 +32,8 @@ import edu.uci.ics.hyracks.algebricks.core.algebra.metadata.IMetadataProvider;
 
 /**
  * @author Xiaoyu Ma
+ *         This class is a type computer for single string as input and returns integer or null
+ *         NULL and String are acceptable argument type and Union as optional string is also ok.
  */
 public class UnaryStringInt32OrNullTypeComputer implements IResultTypeComputer {
 
