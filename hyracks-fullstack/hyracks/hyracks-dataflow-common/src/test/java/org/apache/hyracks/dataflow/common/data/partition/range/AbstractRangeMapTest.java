0d24fc4895d9c675c84cacfa8cb8ed8d14bef5fb
Stephen Ermshar <stephen.ermshar@wallawalla.edu>
2019-06-18 09:35:48 -0700

    remove IRangeMap and its usage in AbstractRangeMapTest

--- hyracks-fullstack/hyracks/hyracks-dataflow-common/src/test/java/org/apache/hyracks/dataflow/common/data/partition/range/AbstractRangeMapTest.java
+++ hyracks-fullstack/hyracks/hyracks-dataflow-common/src/test/java/org/apache/hyracks/dataflow/common/data/partition/range/AbstractRangeMapTest.java
@@ -22,13 +22,12 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.LinkedList;
 
-import org.apache.hyracks.api.dataflow.value.IRangeMap;
 import org.apache.hyracks.api.exceptions.HyracksDataException;
 import org.apache.hyracks.data.std.primitive.LongPointable;
 
 public abstract class AbstractRangeMapTest {
 
-    protected void testMapNumeric(LinkedList<Byte> tags, LinkedList<Long> values, IRangeMap rangeMap)
+    protected void testMapNumeric(LinkedList<Byte> tags, LinkedList<Long> values, RangeMap rangeMap)
             throws HyracksDataException {
         LongPointable lp = (LongPointable) LongPointable.FACTORY.createPointable();
         int columnIndex = 0;
@@ -40,28 +39,14 @@ public abstract class AbstractRangeMapTest {
         int i = 0;
         int splitIndex = 0;
         for (Byte key : tags) {
-            if (i == 0) {
-                // Min
-                assertTrue("Min tag matches", (byte) key == rangeMap.getMinTag(columnIndex));
-                lp.set(rangeMap.getMinByteArray(columnIndex), rangeMap.getMinStartOffset(columnIndex) + 1,
-                        rangeMap.getMinLength(columnIndex) - 1);
-                assertTrue("Min value matches", values.get(i) == lp.getLong());
-            } else if (i == tags.size()) {
-                // Max
-                assertTrue("Max tag matches", (byte) key == rangeMap.getMaxTag(columnIndex));
-                lp.set(rangeMap.getMaxByteArray(columnIndex), rangeMap.getMaxStartOffset(columnIndex) + 1,
-                        rangeMap.getMaxLength(columnIndex) - 1);
-                assertTrue("Max value matches", values.get(i) == lp.getLong());
-            } else {
-                // Splits
-                assertTrue("Tag matches for split: " + splitIndex,
-                        (byte) key == rangeMap.getTag(columnIndex, splitIndex));
-                lp.set(rangeMap.getByteArray(columnIndex, splitIndex),
-                        rangeMap.getStartOffset(columnIndex, splitIndex) + 1,
-                        rangeMap.getLength(columnIndex, splitIndex) - 1);
-                assertTrue("Value matches for split: " + splitIndex, values.get(i) == lp.getLong());
-                splitIndex++;
-            }
+            // check Splits
+            assertTrue("Tag matches for split: " + splitIndex,
+                    (byte) key == rangeMap.getTag(columnIndex, splitIndex));
+            lp.set(rangeMap.getByteArray(),
+                    rangeMap.getStartOffset(columnIndex, splitIndex) + 1,
+                    rangeMap.getLength(columnIndex, splitIndex) - 1);
+            assertTrue("Value matches for split: " + splitIndex, values.get(i) == lp.getLong());
+            splitIndex++;
             ++i;
         }
     }
