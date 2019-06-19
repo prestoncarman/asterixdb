1772b81c2b61555e0f747f94d0a39dd09f6196a2
Stephen Ermshar <stephen.ermshar@wallawalla.edu>
2019-06-19 10:16:07 -0700

    tentatively fixed last exception issue

--- asterixdb/asterix-om/src/main/java/org/apache/asterix/dataflow/data/nontagged/comparators/AGenericAscBinaryComparator.java
+++ asterixdb/asterix-om/src/main/java/org/apache/asterix/dataflow/data/nontagged/comparators/AGenericAscBinaryComparator.java
@@ -28,7 +28,7 @@ final class AGenericAscBinaryComparator extends AbstractAGenericBinaryComparator
     }
 
     @Override
-    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) throws HyracksDataException {
         return compare(leftType, b1, s1, l1, rightType, b2, s2, l2);
     }
 }
